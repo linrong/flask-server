e4c3ca060ca26e98f02c45c8de9ab558a3126dff
linrong <lazyhlr@gmail.com>
2019-08-28 06:51:24 +0000

    Refactor(*):Modify reponse and add http class

---
+++ app/libs/httper.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+"""
+  Created by linrong on 2019/08/28.
+"""
+__author__ = 'linrong'
+
+import requests
+
+class HTTP:
+	@staticmethod
+	def get(url, return_json=True):
+		res = requests.get(url)
+		if res.status_code != 200:
+			return {} if return_json else ''
+		return res.json() if return_json else res.text
