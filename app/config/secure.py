8fef46106d899770738c71a4ccecccf77617850e
linrong <lazyhlr@gmail.com>
2019-08-28 17:02:51 +0800

    Refactor(mysql):add mysql and modify run port

--- app/config/secure.py
+++ app/config/secure.py
@@ -22,6 +22,7 @@ python3+flask-SQLAlchemy+SQLAlchemy+pyMysql(import pymysql)+mysql
 # 开启debug模式,应该只用在开发环境
 DEBUG = True 
 # 数据库连接，加载进falsk的config中后，应该在使用 db.init_app(app)时会使用连接数据库，是Flask-SQLAlchemy扩展需要的
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:159951@localhost:3306/zerd?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://flask:flask.com@db:3306/flask'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 # SECRET_KEY配置变量是通用密钥，可在Flask和多个第三方扩展中使用。如其名所示，加密的强度取决与变量值的机密度。不同的程序使用不同的密钥，而且要保证其他人不知道你所用的字符串
 SECRET_KEY = 'But you, Lord , are a shield around me, my glory, the One who lifts my head high.'
