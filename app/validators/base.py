745093d0cbe425d078e098a09ef5b30210dd05db
linrong <lazyhlr@gmail.com>
2019-08-29 03:10:19 +0000

    Refactor(*):remove upload folder and fix isPositiveInteger

--- app/validators/base.py
+++ app/validators/base.py
@@ -29,4 +29,8 @@ class BaseValidator(Form):
 		return self
 
 	def isPositiveInteger(self, value):
+		try:
+			value=int(value)
+		except ValueError:
+			return False
 		return True if (isinstance(value, int) and value > 0) else False
